57c562fb0be299de7f6fd49c0429714ad7516880
Ckerius <ckerius@yandex.ru>
2023-12-14 04:37:13 +0500

    Code style

--- src/main/java/com/example/frigecontent/model/FoodUnit.java
+++ src/main/java/com/example/frigecontent/model/FoodUnit.java
@@ -8,16 +8,11 @@
 @Table(name = "food_units")
 public class FoodUnit {
 
-    @Id // Указываем, что это поле является идентификатором сущности
-    @GeneratedValue = GenerationType.IDENTITY) // Генерация значения идентификатора сущности
+    @Id
+    @GeneratedValue = GenerationType.IDENTITY)
     private Long id;
-
-    private String name; // Имя продукта/единицы пищи
-
-    private String category; // Категория продукта/единицы пищи
-
-    private LocalDate expirationDate; // Дата окончания срока годности продукта/единицы пищи
-
-    private boolean isInStock; // В наличии ли продукт
-
+    private String name;
+    private String category;
+    private LocalDate expirationDate;
+    private boolean isInStock;
 }
