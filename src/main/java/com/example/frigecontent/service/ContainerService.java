201080d38d387ffe1f72d65b669a2d1cbaa5680d
Ckerius <ckerius@yandex.ru>
2023-12-14 05:12:36 +0500

    Добавление сервисных методов для Контейнеров и Списка покупок

---
+++ src/main/java/com/example/frigecontent/service/ContainerService.java
@@ -0,0 +1,32 @@
+@Data
+@Service
+public class ContainerService {
+
+    private ContainerRepository containerRepository;
+    private FoodUnitRepository foodUnitRepository;
+
+    @Autowired
+    public ContainerService(ContainerRepository containerRepository, FoodUnitRepository foodUnitRepository) {
+        this.containerRepository = containerRepository;
+        this.foodUnitRepository = foodUnitRepository;
+    }
+
+    public List<Container> getAllContainers() {
+        return containerRepository.findAll();
+    }
+
+    public Container createContainer(Container container) {
+        return containerRepository.save(container);
+    }
+
+    public Container addProductToContainer(Long containerId, Long foodUnitId) {
+        Container container = containerRepository.findById(containerId).orElseThrow(() -> new EntityNotFoundException("Container not found"));
+        FoodUnit foodUnit = foodUnitRepository.findById(foodUnitId).orElseThrow(() -> new EntityNotFoundException("FoodUnit not found"));
+
+        container.getProducts().add(foodUnit);
+        return containerRepository.save(container);
+    }
+
+    // Другие методы для управления контейнерами
+
+}
---
+++ src/main/java/com/example/frigecontent/service/ShopingListService.java
@@ -0,0 +1,32 @@
+@Data
+@Service
+public class ShoppingListService {
+
+    private ShoppingListRepository shoppingListRepository;
+    private FoodUnitRepository foodUnitRepository;
+
+    @Autowired
+    public ShoppingListService(ShoppingListRepository shoppingListRepository, FoodUnitRepository foodUnitRepository) {
+        this.shoppingListRepository = shoppingListRepository;
+        this.foodUnitRepository = foodUnitRepository;
+    }
+
+    public List<ShoppingList> getAllShoppingLists() {
+        return shoppingListRepository.findAll();
+    }
+
+    public ShoppingList createShoppingList(ShoppingList shoppingList) {
+        return shoppingListRepository.save(shoppingList);
+    }
+
+    public ShoppingList addProductToShoppingList(Long shoppingListId, Long foodUnitId) {
+        ShoppingList shoppingList = shoppingListRepository.findById(shoppingListId).orElseThrow(() -> new EntityNotFoundException("ShoppingList not found"));
+        FoodUnit foodUnit = foodUnitRepository.findById(foodUnitId).orElseThrow(() -> new EntityNotFoundException("FoodUnit not found"));
+
+        shoppingList.getProducts().add(foodUnit);
+        return shoppingListRepository.save(shoppingList);
+    }
+
+    // Другие методы для управления списками покупок
+
+}
